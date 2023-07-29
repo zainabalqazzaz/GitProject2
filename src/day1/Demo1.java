2ed823d740bab24122163f241bbbfeae74e0578e
zainabalqazzaz <140640712+zainabalqazzaz@users.noreply.github.com>
2023-07-29 11:47:18 -0400

    Update Demo1.java

--- src/day1/Demo1.java
+++ src/day1/Demo1.java
@@ -4,5 +4,10 @@ public class Demo1 {
 public static void main(String[] args) {
 	System.out.println("Test 1");
 }
-
+public static void multiply(int a,int b){
+system.out.println(a*b);
+}
+	
+		
+		
 }
